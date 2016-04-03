5e309c90e90d650d9da77e12e4f1ede0a26bdee5
Glenn Edgar <Glenn Edgar>
2016-04-03 12:55:30 -0700

    original commit

---
+++ cf_events.h
@@ -0,0 +1,20 @@
+/*
+**
+** File cf_events.h
+**
+*/
+
+#ifndef _CF_EVENTS_H_
+#define _CF_EVENTS_H_
+
+
+#define CF_INIT_EVENT        0
+#define CF_TIME_TICK_EVENT   1
+
+
+void cf_initialize_event_manager( void );
+void cf_send_interrupt_event( unsigned event, unsigned data);
+void cf_send_event( unsigned event, unsigned data );
+int cf_rx_event( unsigned *event, unsigned *event_data );
+
+#endif
