9feeb57e8a077393c3a14d7e264118ed20d325f0
Glenn Edgar <Glenn Edgar>
2016-04-04 22:25:30 -0700

    application change

--- cf_external_functions.c
+++ cf_external_functions.c
@@ -26,3 +26,15 @@ int  pat_watch_dog(unsigned link_id, unsigned param_1,
     CyWdtClear();
     return CF_DISABLE;
 }
+
+
+
+int16    temp_die;
+int meaure_die_temperature(unsigned link_id, unsigned param_1,
+  unsigned param_2, unsigned param_3, unsigned event, unsigned data)
+{
+    DieTemp_1_GetTemp(&temp_die);
+    temp_die = ((temp_die*9)/5)+32;                     ;
+
+    return CF_DISABLE;
+}
